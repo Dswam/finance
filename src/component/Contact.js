d04d452f8abd846fcb6e9f720e1ac0a4ae786837
devp1447@gmail.com <devp1447@gmail.com>
2023-02-20 13:58:52 +0530

    fifth-commit

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,50 @@
 import React from 'react'
 import HeroSection from '../pages/HeroSection'
-
+import "../cssComponent/contact.css"
 const Contact = () => {
   return (
-    <HeroSection/>
+    <>
+      <HeroSection />
+      <div className="container backs">
+        <div className="row">
+          <h1>contact us</h1>
+        </div>
+        <div className="row">
+          <h4 style={{textAlign:"center"}}>We'd love to hear from you!</h4>
+        </div>
+        <div className="row input-container">
+          <div className="col-xs-12">
+            <div className="styled-input wide">
+              <input type="text" required />
+              <label>Name</label>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <div className="styled-input">
+              <input type="text" required />
+              <label>Email</label>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <div className="styled-input" style={{float:"right"}}>
+              <input type="text" required />
+              <label>Phone Number</label>
+            </div>
+          </div>
+          <div className="col-xs-12">
+            <div className="styled-input wide">
+              <textarea required></textarea>
+              <label>Message</label>
+            </div>
+          </div>
+          <div className="col-xs-12">
+            <div className="btn-lrg submit-btn">Send Message</div>
+          </div>
+        </div>
+      </div>
+
+    </>
+
   )
 }
 
