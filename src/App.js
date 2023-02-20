3e53730c56ea78f4e6a320da1334d80c73d16e76
devp1447@gmail.com <devp1447@gmail.com>
2023-02-20 15:13:30 +0530

    sixth-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { Suspense, lazy } from 'react';
+import React, { Suspense} from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Header from './component/Header';
-import About from './component/About';
-import Contact from './component/Contact';
-import Login from './component/Login';
 import Footer from './component/Footer';
-
+// import { ErrorBoundary } from 'react-error-boundary';
+// import ErrorFallback from './component/ErrorBoundary';
+const Home=React.lazy(()=>import("./pages/Home"))
+const About=React.lazy(()=>import("./component/About"))
+const Login=React.lazy(()=>import("./component/Login"))
+const Contact=React.lazy(()=>import("./component/Contact"))
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Header />
+        <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/benefits" element={<Login />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </>
